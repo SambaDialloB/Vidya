35dbfd85c58cb7d73b22413bcc169fba5f6961a2
Samba Diallo <sdiallo@csumb.edu>
2017-03-23 14:40:48 +0000

    made a simple store front, which links to a page that sorts the games, sorting is not done yet

---
+++ storeFront.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Vidya Games</title>
+    </head>
+    
+    <body>
+        RUBRIC
+        <br>
+        1) Database has at least 3 tables with 40 records (10 points) <br>
+        2) Users can filter data using at least three fields (15 points) <br>
+        3) Users can sort results (asc,desc) using at least one field (10 points) <br>
+        4) Users can click on an item to get further info (10 points) <br>
+        5) Users can add items to shopping cart using a Session (10 points) <br>
+        6) Users can see the content of the shopping cart (10 points) <br>
+        7) The web pages have a nice and consistent look and feel (10 points) <br>
+        8) The team used Github for collaboration (10 points) <br>
+        9) The team used Trello or a similar tool for project management (10 points) <br>
+        10) In a Word document include User Story, Database schema, and mock up (5 points) UPLOAD these documents here and ALSO link them from your C9 site  (5 points) <br>
+        <h1> Welcome to the Vidya Game story!</h1>
+        How would you like to see the games?
+        <form method="post" action="gameDisplay.php" name="choice">
+            <input type="submit" name="mat" value="Maturity!"/>
+            <input type="submit" name="con" value="Console!"/>
+            <input type="submit" name="rat" value="Rating!"/>
+        </form>
+    </body>
+</html>
